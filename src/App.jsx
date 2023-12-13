a7720391a32ac845808eb58fd5853d40ecd49c50
Hafid Firman febrian <120733853+fakeplastic3s@users.noreply.github.com>
2023-12-13 18:44:31 +0700

    change format photo

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ function App() {
       <div className="w-full bg-slate-50 py-24 px-4">
         <h1 className="font-bold pb-5 text-xl uppercase text-center">New Collection</h1>
         <div className="max-w-[1240px] mx-auto flex flex-wrap gap-10 justify-center items-center ">
-          <Card className="max-w-sm" imgSrc="/produk/1.JPG" horizontal>
+          <Card className="max-w-sm" imgSrc="/produk/1.webp" horizontal>
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nama Produk Baru 1</h5>
             <p className="font-normal text-gray-700 dark:text-gray-400">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quia eaque odio tempora accusamus quibusdam modi aspernatur laboriosam eveniet facilis commodi dolor, sequi unde itaque, dolorem consectetur officiis dolores
@@ -79,7 +79,7 @@ function App() {
             </p>
             <h6 className="text-lg font-bold tracking-tight text-gray-900 dark:text-white">$ 13,4</h6>
           </Card>
-          <Card className="max-w-sm" imgSrc="/produk/2.JPG" horizontal>
+          <Card className="max-w-sm" imgSrc="/produk/2.webp" horizontal>
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nama Produk Baru 2</h5>
             <p className="font-normal text-gray-700 dark:text-gray-400">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quia eaque odio tempora accusamus quibusdam modi aspernatur laboriosam eveniet facilis commodi dolor, sequi unde itaque, dolorem consectetur officiis dolores
@@ -87,7 +87,7 @@ function App() {
             </p>
             <h6 className="text-lg font-bold tracking-tight text-gray-900 dark:text-white">$ 8,1</h6>
           </Card>
-          <Card className="max-w-sm" imgSrc="/produk/3.JPG" horizontal>
+          <Card className="max-w-sm" imgSrc="/produk/3.webp" horizontal>
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
             <p className="font-normal text-gray-700 dark:text-gray-400">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quia eaque odio tempora accusamus quibusdam modi aspernatur laboriosam eveniet facilis commodi dolor, sequi unde itaque, dolorem consectetur officiis dolores
