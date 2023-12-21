121868e28b7e4734b73a6a9917271afc62f60d9d
Hafid Firman febrian <120733853+fakeplastic3s@users.noreply.github.com>
2023-12-21 14:35:08 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -172,7 +172,7 @@ function App() {
       </div>
 
       {/* Location */}
-      <div className="w-full bg-slate-50 py-24 px-4">
+      <div className="w-full py-24 px-4">
         <h1 className="font-bold pb-5 text-xl uppercase text-center">Location</h1>
 
         <div className="max-w-[1240px] mx-auto flex flex-wrap gap-10 lg:justify-between justify-center items-center ">
