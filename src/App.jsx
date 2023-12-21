67e08c02cb19862ec014cdd5b5b62dbc63fed5ce
Hafid Firman febrian <120733853+fakeplastic3s@users.noreply.github.com>
2023-12-21 13:30:59 +0700

    update produk

--- src/App.jsx
+++ src/App.jsx
@@ -140,48 +140,8 @@ function App() {
           <h1 className="font-bold text-xl uppercase text-center">Collection</h1>
         </div>
         <div className="max-w-[1240px] mx-auto flex flex-wrap gap-10 justify-center items-center ">
-          <Card className="max-w-sm" imgSrc="/produk/4.jpg" horizontal>
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Outer</h5>
-          </Card>
-          <Card className="max-w-sm" imgSrc="/produk/2.webp" horizontal>
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nama Produk 2</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quia eaque odio tempora accusamus quibusdam modi aspernatur laboriosam eveniet facilis commodi dolor, sequi unde itaque, dolorem consectetur officiis dolores
-              dignissimos.
-            </p>
-            <h6 className="text-lg font-bold tracking-tight text-gray-900 dark:text-white">$ 8,1</h6>
-          </Card>
-          <Card className="max-w-sm" imgSrc="/produk/3.webp" horizontal>
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quia eaque odio tempora accusamus quibusdam modi aspernatur laboriosam eveniet facilis commodi dolor, sequi unde itaque, dolorem consectetur officiis dolores
-              dignissimos.
-            </p>
-            <h6 className="text-lg font-bold tracking-tight text-gray-900 dark:text-white">$ 8,1</h6>
-          </Card>
-          <Card className="max-w-sm" imgSrc="/produk/1.webp" horizontal>
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nama Produk 1</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quia eaque odio tempora accusamus quibusdam modi aspernatur laboriosam eveniet facilis commodi dolor, sequi unde itaque, dolorem consectetur officiis dolores
-              dignissimos.
-            </p>
-            <h6 className="text-lg font-bold tracking-tight text-gray-900 dark:text-white">$ 13,4</h6>
-          </Card>
-          <Card className="max-w-sm" imgSrc="/produk/2.webp" horizontal>
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nama Produk 2</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quia eaque odio tempora accusamus quibusdam modi aspernatur laboriosam eveniet facilis commodi dolor, sequi unde itaque, dolorem consectetur officiis dolores
-              dignissimos.
-            </p>
-            <h6 className="text-lg font-bold tracking-tight text-gray-900 dark:text-white">$ 8,1</h6>
-          </Card>
-          <Card className="max-w-sm" imgSrc="/produk/3.webp" horizontal>
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, quia eaque odio tempora accusamus quibusdam modi aspernatur laboriosam eveniet facilis commodi dolor, sequi unde itaque, dolorem consectetur officiis dolores
-              dignissimos.
-            </p>
-            <h6 className="text-lg font-bold tracking-tight text-gray-900 dark:text-white">$ 8,1</h6>
+          <Card className="max-w-sm" imgAlt="foto produk" imgSrc="/produk/4.JPG">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">Outer</h5>
           </Card>
         </div>
         <div id="Location"></div>
